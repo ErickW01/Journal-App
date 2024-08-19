22c9b944ffb8afa918c70e325df951bbbd6f410f
ErickW01 <36089754+ErickW01@users.noreply.github.com>
2024-08-19 16:43:18 -0700

    created basic log in component

---
+++ app/components/Nav.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Nav() {
+  return (
+   <section className='bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10'>
+         <div className='prose prose-xl mx-auto flex justify-between flex-col sm:flex-row'>
+            <h1 className='text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0'>
+            <Link href='/'className='text-white/90 no-underline hover:text-white'>
+                Write It Down
+            </Link>
+            </h1>
+        </div>
+   </section>
+  )
+}
