0b5fc114f2e1c723fd5f13316dcb72fd1e099288
ErickW01 <36089754+ErickW01@users.noreply.github.com>
2024-08-23 12:23:20 -0700

    intro of Hero Section

--- app/components/LogIn.tsx
+++ app/components/LogIn.tsx
@@ -1,11 +1,20 @@
+'use client'
+
 import React from 'react'
 
 export default function LogIn() {
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    console.log('handleSubmit - Successful Login')
+
+  }
   return (
     <section className='my-0 mx-auto h-screen
       bg-slate-800
       flex items-center'>
-    <form className="max-w-sm mx-auto w-full">
+    <form className="max-w-sm mx-auto w-full" onSubmit={handleSubmit}>
       <div className="mb-5">
         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-200 dark:text-white">Your email</label>
         <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Nav from "./components/Nav";
 import LogIn from "./components/LogIn";
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <main className="bg-slate-600">
       <Nav/>
+      {/* <Hero/> */}
       <LogIn/>
     </main>
   );
