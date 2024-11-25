5405b6ee37786bccaf33d73e3832a718c393f9b2
ErickW01 <36089754+ErickW01@users.noreply.github.com>
2024-11-24 17:55:52 -0800

    removed bad component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Nav from "../components/Nav";
-import LogIn from "../components/LogIn";
 import Hero from "../components/Hero/Hero";
 
 export default function Home() {
