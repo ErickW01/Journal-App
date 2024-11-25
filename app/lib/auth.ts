ded38c8c0856217d1f8f7ad6c4ddc1f0403207b4
ErickW01 <36089754+ErickW01@users.noreply.github.com>
2024-11-24 17:53:42 -0800

    got auth working

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -6,7 +6,8 @@ export const auth = betterAuth({
     socialProviders: {
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+            redirectURI: process.env.BASE_URL + '/api/auth/callback/google'
         }
     }
 })
--- components/Btn/SignInBtn.tsx
+++ components/Btn/SignInBtn.tsx
@@ -1,17 +1,17 @@
 "use client"
 import React from "react";
-import { authClient } from "@/app/lib/auth-client";
+import { signIn } from "@/app/lib/auth-client";
 
 export default function SignInBtn() {
-    const signIn = async() => {
-        await authClient.signIn.social({
-            provider: "google",
-            callbackURL: '/personal-journal'
-        });
-    };
+
     return(
         <div>
-            <button onClick={signIn}>
+            <button onClick={async() => {
+                await signIn.social({
+                    provider: "google",
+                    callbackURL: '/personal-journal'
+                })
+            }}>
                 Sign In w/ Google
             </button>
         </div>
